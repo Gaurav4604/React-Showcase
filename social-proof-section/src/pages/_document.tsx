f1f52f8dd4225aa99c09e4fa085401631abf4d95
Gaurav Singh <gp5in9h@gmail.com>
2023-04-13 10:57:07 +0530

    1) Added Config for base mui + nextjs app 2) Added color and font theme to theme Config

---
+++ social-proof-section/src/pages/_document.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "../utils/createEmotionCache";
+import { AppType } from "next/app";
+import { EmotionCache } from "@emotion/react";
+
+export default class MyDocument extends Document<{
+  emotionStyleTags: JSX.Element[];
+}> {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>{this.props.emotionStyleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (
+        App: AppType | React.ComponentType<{ emotionCache: EmotionCache }>
+      ) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
--- social-proof-section/src/theme.ts
+++ social-proof-section/src/theme.ts
@@ -1,5 +1,61 @@
+import { leagueSpartan } from "./pages/_app";
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({});
+import "@mui/material";
+interface customPaletteOptions {
+  veryDarkMagenta?: string;
+  softPink?: string;
+  darkGrayishMagenta?: string;
+  lightGrayishMagenta?: string;
+  white?: string;
+}
 
-export default theme;
+interface customPalette {
+  veryDarkMagenta: string;
+  softPink: string;
+  darkGrayishMagenta: string;
+  lightGrayishMagenta: string;
+  white: string;
+}
+
+declare module "@mui/material" {
+  interface Palette {
+    custom?: customPalette;
+  }
+  interface PaletteOptions {
+    custom?: customPaletteOptions;
+  }
+}
+
+const rootTheme = createTheme({
+  palette: {
+    custom: {
+      veryDarkMagenta: "hsl(300, 43%, 22%)",
+      softPink: "hsl(333, 80%, 67%)",
+      darkGrayishMagenta: "hsl(303, 10%, 53%)",
+      lightGrayishMagenta: "hsl(300, 24%, 96%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          fontFamily: leagueSpartan.style.fontFamily,
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        root: ({ theme }) =>
+          theme.unstable_sx({
+            [theme.breakpoints.up("lg")]: {
+              maxWidth: "100vw",
+            },
+          }),
+      },
+    },
+  },
+});
+
+export default rootTheme;
