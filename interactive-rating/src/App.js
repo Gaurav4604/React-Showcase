62804a26cfe00e2bbce2adb8e25446d76034ebd3
Gaurav Singh <gp5in9h@gmail.com>
2023-09-10 14:36:29 +0530

    dir structure changed

--- interactive-rating/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Routes from "./Routing";
-
-const App = () => {
-  return <Routes />;
-};
-
-export default App;
--- stats-preview-card/src/index.js
+++
@@ -1,5 +0,0 @@
-import reactDom from "react-dom";
-import "./index.css";
-import App from "./App";
-
-reactDom.render(<App />, document.getElementById("root"));
