b0622cdcf7b3c54b91f0f1e8af8464516e35dbdf
Gaurav Singh <gp5in9h@gmail.com>
2023-09-23 20:40:34 +0530

    base components created

---
+++ Junior/age-calculator-app/src/LayoutCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const LayoutCard = (props: Props) => {
+  return <div>LayoutCard</div>;
+};
+
+export default LayoutCard;
---
+++ Junior/age-calculator-app/src/ResultTypography.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ResultTypography = (props: Props) => {
+  return <div>ResultTypography</div>;
+};
+
+export default ResultTypography;
