c2e0b3da0cdf89b7cc3e4d21302dbd98dc5527dc
Gaurav Singh <gp5in9h@gmail.com>
2023-10-04 19:39:59 +0530

    updated app for desktop

--- Junior/age-calculator-app/src/LayoutCard.tsx
+++ Junior/age-calculator-app/src/LayoutCard.tsx
@@ -1,6 +1,7 @@
-import { Card } from "@mui/material";
+import { Card, Stack } from "@mui/material";
 import DateInput from "./DateInput";
 import IconButtonAndDivider from "./IconButtonAndDivider";
+import ResultTypography from "./ResultTypography";
 
 const LayoutCard = () => {
   return (
@@ -17,13 +18,35 @@ const LayoutCard = () => {
       }}
       elevation={0}
     >
-      <DateInput
-        placeholder="DD"
-        label="DAY"
-        // error
-        // errorHandler={() => "Must be a valid date"}
-      />
+      <Stack
+        direction={"row"}
+        sx={{ width: "85%" }}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <DateInput
+          placeholder="DD"
+          label="DAY"
+          // error
+          // errorHandler={() => "Must be a valid date"}
+        />
+        <DateInput
+          placeholder="MM"
+          label="MONTH"
+          // error
+          // errorHandler={() => "Must be a valid date"}
+        />
+        <DateInput
+          placeholder="YYYY"
+          label="YEAR"
+          // error
+          // errorHandler={() => "Must be a valid date"}
+        />
+      </Stack>
       <IconButtonAndDivider />
+      <ResultTypography highlighted="--" text="years" />
+      <ResultTypography highlighted="--" text="months" />
+      <ResultTypography highlighted="--" text="days" />
     </Card>
   );
 };
--- Junior/age-calculator-app/src/ResultTypography.tsx
+++ Junior/age-calculator-app/src/ResultTypography.tsx
@@ -1,9 +1,31 @@
-import React from "react";
-
-type Props = {};
+import { Typography } from "@mui/material";
+import "@fontsource/poppins/700-italic.css";
+type Props = {
+  highlighted: string;
+  text: string;
+};
 
 const ResultTypography = (props: Props) => {
-  return <div>ResultTypography</div>;
+  return (
+    <Typography
+      sx={{
+        fontFamily: "poppins",
+        fontStyle: "italic",
+        fontWeight: "700",
+        fontSize: "4rem",
+      }}
+    >
+      <span
+        style={{
+          color: "hsl(259, 100%, 65%)",
+          letterSpacing: "0.2rem",
+        }}
+      >
+        {props.highlighted}{" "}
+      </span>
+      {props.text}
+    </Typography>
+  );
 };
 
 export default ResultTypography;
